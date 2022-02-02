827633af5302ca936728f4e96e375046e25721de
BiagioBruno5 <biagio.bruno@si2001.it>
2022-02-02 11:39:27 +0100

    commit

--- RentalCar/src/main/java/com/RentalCar/Application.java
+++ RentalCar/src/main/java/com/RentalCar/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RentalCarApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentalCarApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
